c89b35c39a76ad688e24f153163a8f3f3d184d77
mahimagupta24 <mahimagupta2401@gmail.com>
2023-05-20 17:19:24 +0530

    add functionality in cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,15 +4,15 @@ import { useEffect, useState } from "react";
 export default function Cart() {
   const [cartItems, setCartItems] = useState([]);
   const fetchCartDetails = () => {
-    const token =
+    const token=
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI0ODI4MzFlMC02ODUxLTQ1NGQtYTQyNC04ODJiMmJiNGE5MjkiLCJlbWFpbCI6ImFkYXJzaGJhbGlrYUBnbWFpbC5jb20ifQ.dug-ofAz7IuYiDLCVZRVaaOl_TuUPoT-fxbUN9uKkvw";
     axios
       .get("/api/user/cart", {
         headers: {
-          Authorization: token,
+          authorization: `bearer ${token}`,
         },
       })
-      .then((resp) => setCartItems(resp.data))
+      .then((resp) => console.log(resp))
       .catch((e) => console.error(e));
   };
 
