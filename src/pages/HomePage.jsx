65d66c501ad8ded33a4cd6f35856a6043ba97d35
mahimagupta24 <mahimagupta2401@gmail.com>
2023-05-30 21:28:35 +0530

    add images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,32 +1,30 @@
 import { useContext, useEffect, useState } from "react";
-import { useNavigate,Link, useLocation} from "react-router-dom";
-import Footer from "../components/Footer.js"
+import { useNavigate, Link, useLocation } from "react-router-dom";
+import Footer from "../components/Footer.js";
 import Header from "../components/Header";
 import { ProductContext } from "../context/ProductContext.js";
 
 export default function Home() {
-  const{dispatch}=useContext(ProductContext)
+  const { dispatch } = useContext(ProductContext);
   const navigate = useNavigate();
   // const location = useLocation()
   const [categories, setCategories] = useState([]);
 
   const [isLoading, setIsLoading] = useState(true);
-  
-  
 
   const loadCategories = async () => {
-    try{
-    const response = await fetch("/api/categories");
-    if(response.status===200){
-      const data = await response.json();
-      const categories = data.categories;
-      setCategories(categories);
+    try {
+      const response = await fetch("/api/categories");
+      if (response.status === 200) {
+        const data = await response.json();
+        const categories = data.categories;
+        setCategories(categories);
+      }
+    } catch (e) {
+      console.error(e);
     }
-  }catch(e){
-    console.error(e);
-  }
-  setIsLoading(false);
-}
+    setIsLoading(false);
+  };
 
   useEffect(() => {
     loadCategories();
@@ -36,42 +34,47 @@ export default function Home() {
     navigate("/products");
   };
 
+  const handleCategoryChange = (categoryName) => {
+    dispatch({
+      type: "SET_SELECTED_CATEGORY",
+      payload: [categoryName],
+    });
+    navigate("/products");
+  };
 
+  return (
+    <div>
+      <Header />
+      {isLoading && <p>Loading...</p>}
+      {!isLoading &&
+        categories.map((category) => {
+          const { _id, categoryName, description, imageUrl } = category;
 
-  
-    // const handleCategoryChange = (categoryName) => {
-    //   setSelectedCategory(selectedCategory === categoryName ? null : categoryName);
-    //   navigate('/products');
-    // };
-    
-    // const handleCategoryChange = (categoryName) => {
-    //   const updatedCategories=selectedCategories.includes(categoryName)?
-    //  selectedCategories.filter((cat)=>cat!==categoryName):[...selectedCategories,categoryName]
-    //   setSelectedCategories(updatedCategories)
-    //   navigate('/products')
-    //  }
-    
-      
-  
-  
-    return (
-      <div>
-        <Header/>
-        {isLoading&&<p>Loading...</p>}
-        {!isLoading &&categories.map((category) => {
-          const { _id, categoryName, description } = category;
           return (
-            <div key={_id}>
+            <div
+              className="main-container"
+              key={_id}
+              style={{ display: "flex",flexDirection:"row"}}
+              onClick={() => handleCategoryChange(categoryName)}
+            >
+              <img
+                src={imageUrl}
+                alt={categoryName}
+                width="200px"
+                height="100px"
+              />
               <h4>{categoryName} collection</h4>
               <p>{description}</p>
-              <Link to="/products" onClick={()=>dispatch({type:"SET_SELECTED_CATEGORY",payload:[categoryName]})}>go </Link>
 
               {/* <Link to={{pathname:"/products",state:{from:location.pathname}}} onClick={()=>dispatch({type:"SET_SELECTED_CATEGORY",payload:[categoryName]})}>go </Link> */}
             </div>
           );
         })}
-        <button onClick={clickHandler}>Shop now</button>
-        <Footer/>
+        <div onClick={clickHandler}>
+          <img src="https://m.media-amazon.com/images/S/aplus-media-library-service-media/138b2b0c-b88e-4667-95dd-9f72b41e94d1.__CR0,0,1464,625_PT0_SX1464_V1___.jpg" height="350px"width="100%"alt="fashion"/>
+      
       </div>
-    );
+      <Footer />
+    </div>
+  );
 }
