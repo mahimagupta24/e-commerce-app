19702eb19e56ae8ddf26383c11f0d93cff7bda5b
mahimagupta24 <mahimagupta2401@gmail.com>
2023-05-30 21:29:14 +0530

    add images

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -9,19 +9,16 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "men",
-    description:
-      "checkout the latest men collection",
+      imageUrl:'https://images.unsplash.com/photo-1632226390535-2f02c1a93541?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG1lbiUyMGZhc2hpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
   },
   {
     _id: uuid(),
     categoryName: "women",
-    description:
-      "checkout the latest women collection",
+      imageUrl:'https://tse4.mm.bing.net/th?id=OIP.UWRNRJcB0bMEMZ0WuoQ8PgHaGH&pid=Api&P=0&h=180'
   },
   {
     _id: uuid(),
     categoryName: "kids",
-    description:
-      "checkout the latest kids collection",
+      imageUrl:'https://i.pinimg.com/originals/61/69/5d/61695d9162eb15614d82ec6bd011501d.jpg'
   },
 ];
