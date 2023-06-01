7d07098275d76972a7e406f36c59720a87ab3b9a
mahimagupta24 <mahimagupta2401@gmail.com>
2023-06-01 11:25:41 +0530

    add styles

--- src/pages/ProductsListingPage.jsx
+++ src/pages/ProductsListingPage.jsx
@@ -24,57 +24,64 @@ export default function Products() {
   return (
     <div>
       <Header />
+      <div><Filters/></div>
       {/* {filteredProducts&&<div><h2>Products:</h2> */}
-      {filteredPriceProducts.map((product) => {
-        const { _id, img, original_price, price, rating, name } = product;
-        // const isCartProductPresent = cartProducts.some(
-        //   (cartProduct) => cartProduct?._id === product?._id
-        // );
+      <ul className="product-card" >
+      {/* <div className="product-card"> */}
+        {filteredPriceProducts.map((product) => {
+          const { _id, img, original_price, price, rating, name } = product;
+          // const isCartProductPresent = cartProducts.some(
+          //   (cartProduct) => cartProduct?._id === product?._id
+          // );
 
-        const handleAddCartItems = (product) => {
-          isLoggedIn ? addCartItems(product) : navigate("/login");
-        };
-
-        return (
-          <div key={_id}>
-            <Link to={`/product/${_id}`}>
-              <div className="product-card" key={_id}></div>
-            </Link>
-            <img className="product-img" src={img} alt={name} />
-            <h4>{name}</h4>
-            <p>Original Price: ${original_price}</p>
-            <p>Price: ${price}</p>
-            <p>Rating: {rating}</p>
-
-            {isCartProductPresent(_id) ? (
-              <Link to="/cart">
-                <button className="cart-btn">Go to cart</button>
+          const handleAddCartItems = (product) => {
+            isLoggedIn ? addCartItems(product) : navigate("/login");
+          };
+          
+            return (
+               <li className="products-list" key ={_id.id}>
+                 {/* <div className="products-list" key ={_id.id}> */}
+              <Link to={`/product/${_id}`}>
               </Link>
-            ) : (
-              <button
-                className="cart-btn"
-                onClick={() => handleAddCartItems(product)}
-              >
-                Add to cart
-              </button>
-            )}
-            {isWishlistProductPresent(_id) ? (
-              <Link to="/wishlist">
-                <button className="wishlist-btn">Go to wishlist</button>
-              </Link>
-            ) : (
-              <button
-                className="wishlist-btn"
-                onClick={() => handleAddWishlistItems(product)}
-              >
-                Add to wishlist
-              </button>
-            )}
-          </div>
-        );
-      })}
+              <img className="product-img" src={img} alt={name} />
+              <h4>{name}</h4>
+              <p>Original Price: ${original_price}</p>
+              <p>Price: ${price}</p>
+              <p>Rating: {rating}</p>
 
-      <Filters />
+              {isCartProductPresent(_id) ? (
+                <Link to="/cart">
+                  <button className="cart-btn">Go to cart</button>
+                </Link>
+              ) : (
+                <button
+                  className="cart-btn"
+                  onClick={() => handleAddCartItems(product)}
+                >
+                  Add to cart
+                </button>
+              )}
+              {isWishlistProductPresent(_id) ? (
+                <Link to="/wishlist">
+                  <button className="wishlist-btn">Go to wishlist</button>
+                </Link>
+              ) : (
+                <button
+                  className="wishlist-btn"
+                  onClick={() => handleAddWishlistItems(product)}
+                >
+                  Add to wishlist
+                </button>
+              )}
+              {/* </div> */}
+              </li>
+           
+            )
+          
+        })}
+        {/* </div> */}
+      </ul>
+     
     </div>
   );
 }
