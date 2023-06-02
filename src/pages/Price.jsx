cc57b35e8c3ad802416e67c950a35f69b0171d33
mahimagupta24 <mahimagupta2401@gmail.com>
2023-06-02 21:43:53 +0530

    add quantity

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -1,29 +1,49 @@
-import { useContext, useState } from "react"
-import { CartContext } from "../context/CartContext"
-import { useNavigate } from "react-router-dom"
-import "./Price.css"
+import { useContext, useState } from "react";
+import { CartContext } from "../context/CartContext";
+import { useNavigate } from "react-router-dom";
 
-export default function Price(){
-  
-  const navigate = useNavigate()
-    const {cartProducts} = useContext(CartContext)
-    const[discount,setDiscount] = useState(0)
+import { ProductContext } from "../context/ProductContext";
 
-    const totalPrice = cartProducts.reduce((acc, curr) => acc + curr.price*curr.qty, 0);
-    console.log(totalPrice);
-  
-    const grandTotal = totalPrice-discount
-    console.log(grandTotal)
+export default function Price() {
+  const navigate = useNavigate();
+  const { cartProducts } = useContext(CartContext);
 
-    const checkoutHandler = ()=>{
-      navigate("/address")
-    }
+  const [discount, setDiscount] = useState(0);
 
-    return <div className="price-card">
-        <button onClick ={()=>setDiscount(55)}>Apply coupon</button>
-      <p>Total items:{cartProducts.length}</p>
-      <p>discount:{discount}</p>
-      <p>Total Price:{grandTotal}</p> 
-      <button onClick={checkoutHandler}>Checkout</button>
+  const totalPrice = cartProducts.reduce(
+    (acc, curr) => acc + curr.price * curr.qty,
+    0
+  );
+  console.log(totalPrice);
+
+  const qty = cartProducts.reduce((acc, curr) => acc + curr.qty, 0);
+  const grandTotal = totalPrice - discount;
+  console.log(grandTotal);
+
+  const checkoutHandler = () => {
+    navigate("/address");
+  };
+
+  return (
+    <div className="price-card">
+      <div className="coupon">
+        <p className="coupon-heading">Have a coupon?
+        <button className="coupon-btn" onClick={() => setDiscount(55)}>
+          Apply coupon
+        </button>
+        </p>
+        </div>
+
+      <div className="price-description">
+        <p>Total items:{qty}</p>
+        <p>discount:₹{discount}</p>
+        <p>Total Price:₹{grandTotal}</p>
+      </div>
+      <div>
+        <button className="checkout-btn" onClick={checkoutHandler}>
+          Checkout
+        </button>
+      </div>
     </div>
+  );
 }
