64639d0299c26a54cf41f290e26a10b299899227
mahimagupta24 <mahimagupta2401@gmail.com>
2023-06-02 14:01:19 +0530

    add links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,10 @@ export default function Footer(){
     return<footer className="footer-main">
         <p>Made by Mahima Gupta</p>
         <div className="footer-links">
-         <a className="twitter" href="https://twitter.com/MahimaG56804266">twitter</a>
-         <a  className="linked-in"href="https://www.linkedin.com/in/mahimagupta2401/">LinkedIn</a>
-         <a className="github" href="https://github.com/mahimagupta24/">github</a>
+            
+         <i className="fa fa-twitter"><a className="link" href="https://twitter.com/MahimaG56804266"></a></i>
+         <i className="fa fa-linkedin"><a  className="link"href="https://www.linkedin.com/in/mahimagupta2401/"></a></i>
+        <i className="fa fa-github"> <a className="link" href="https://github.com/mahimagupta24/"></a></i>
          </div>
          <p>© copyright 2023 </p>
         
