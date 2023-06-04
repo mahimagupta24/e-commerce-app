492d0f3966403fc3cf996faa79f6be401fd28921
mahimagupta24 <mahimagupta2401@gmail.com>
2023-06-04 11:08:39 +0530

    add  address card style

--- src/pages/AddressForm.jsx
+++ src/pages/AddressForm.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { AddressContext } from "../context/AddressContext";
+import "./AddressForm.css";
 
 export default function UserAddress({ onSave, editingAddressId }) {
   const { dispatch, state } = useContext(AddressContext);
@@ -28,41 +29,41 @@ export default function UserAddress({ onSave, editingAddressId }) {
 
   const submitHandler = (e) => e.preventDefault();
   return (
-    <div>
+    <div className="input-container">
       {editingAddressId === null ? <h4>Add address</h4> : <h4>Edit address</h4>}
-      <form onSubmit={submitHandler}>
-        <input
+      <form onSubmit={submitHandler} className="details">
+        <input className="input-field"
           type="text"
           placeholder="Enter Name"
           value={getAddress?.fullName}
           onChange={(e) => setAddress({ ...address, fullName: e.target.value })}
         />
-        <input
+        <input  className="input-field"
           placeholder="Enter House No,road,colony"
           value={getAddress?.home_address}
           onChange={(e) =>
             setAddress({ ...address, home_address: e.target.value })
           }
         />
-        <input
+        <input  className="input-field"
           type="text"
           value={getAddress?.state}
           placeholder="Enter State"
           onChange={(e) => setAddress({ ...address, state: e.target.value })}
         />
-        <input
+        <input className="input-field"
           type="text"
           value={getAddress?.country}
           placeholder="Enter Country"
           onChange={(e) => setAddress({ ...address, country: e.target.value })}
         />
-        <input
+        <input className="input-field"
           type="number"
           placeholder="Enter Postal Code"
           value={getAddress?.pincode}
           onChange={(e) => setAddress({ ...address, pincode: e.target.value })}
         />
-        <input
+        <input className="input-field"
           type="number"
           placeholder="Enter Mobile no."
           value={getAddress?.phoneNo}
