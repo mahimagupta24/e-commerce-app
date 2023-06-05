9caf817d6cfe13f77ceb27bc0cd23c63e3b28a37
mahimagupta24 <mahimagupta2401@gmail.com>
2023-06-05 17:38:38 +0530

    add styles to logout  page

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -12,7 +12,8 @@ export default function Logout() {
     <div className="logout-container">
       <h1>Logout</h1>
       <div className="logout-card">
-      <h3> You are already logged in </h3>
+        <h2>Hey there!</h2>
+      <h3> You are already logged In </h3>
     
       <button className="logout-btn"onClick={() => logOutHandler()}>Log out</button>
       </div>
