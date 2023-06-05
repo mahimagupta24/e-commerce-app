c1214d661ca0c22f86d69e2741e6fec07746a941
mahimagupta24 <mahimagupta2401@gmail.com>
2023-06-05 20:33:17 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
@@ -13,7 +13,9 @@ import AuthProvider from "./context/AuthContext";
 // Call make Server
 makeServer();
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <Router>
       <ProductProvider>
@@ -31,5 +33,4 @@ ReactDOM.render(
       </ProductProvider>
     </Router>
   </React.StrictMode>,
-  document.getElementById("root")
 );
